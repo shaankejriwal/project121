38619cc2bf13f832cd41c9abd8f777e9507aab5d
shaankejriwal <70429914+shaankejriwal@users.noreply.github.com>
2022-03-05 19:01:58 +0530

    Add files via upload

--- blackscreen.py
+++ blackscreen.py
@@ -1,36 +1,31 @@
-import cv2
-import time
-import numpy as np
-
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output_file = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
-
-cap = cv2.VideoCapture(0)
-
-time.sleep(2)
-bg = 0
-
-for i in range(60):
-    ret, bg = cap.read()
-bg = np.flip(bg, axis=1)
-
-while (cap.isOpened()):
-    ret, img = cap.read()
-    if not ret:
-        break
-    img = np.flip(img, axis=1)
-
-    frame = cv2.resize(frame, (640, 480))
-    img = cv2.resize(img, (640, 480))
-
-    u_black = np.array([104,153,70])
-    l_black = np.array([30,30,0])
-    mask = cv2.inRange(frame, u_black, l_black)
-    res = cv2.bitwise_and(frame, frame, mask = mask)
-
-    f = frame - res
-    f = np.where(f == 0, img, f)
-
-
-cap.release()
-cv2.destroyAllWindows()
+import cv2  
+import numpy as np  
+  
+video = cv2.VideoCapture(0) 
+image = cv2.imread("me.jpeg") 
+  
+while True: 
+  
+    ret, frame = video.read() 
+    print(frame)
+    frame = cv2.resize(frame, (640, 480)) 
+    image = cv2.resize(image, (640, 480)) 
+  
+  
+    u_black = np.array([104, 153, 70]) 
+    l_black = np.array([30, 30, 0]) 
+  
+    mask = cv2.inRange(frame, l_black, u_black) 
+    res = cv2.bitwise_and(frame, frame, mask = mask) 
+  
+    f = frame - res 
+    f = np.where(f == 0, image, f) 
+  
+    cv2.imshow("video", frame) 
+    cv2.imshow("mask", f) 
+  
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+  
+video.release() 
+cv2.destroyAllWindows() 
